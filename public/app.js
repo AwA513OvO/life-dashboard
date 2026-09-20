0a67af591bf91b600fc9905e6fe61e0a19e42978
AwA513OvO <Carpenter513@outlook.com>
2026-09-20 21:34:46 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -487,7 +487,7 @@ function startLongPress(id) {
     longPressTimer = setTimeout(() => {
         longPressTimer = null;
         editTodo(id);
-    }, 1800);
+    }, 600);
     // 记录起始位置，用于判断是否滑动
     window._longPressStart = { x: startX, y: startY };
 }
